5483c4593569610a4b436e76079c6a9f9b98a535
jsoller <jsoller5354@gmail.com>
2018-03-19 22:18:52 -0400

    product to products
    change table name to products from product

--- app/db/productSchema.sql
+++ app/db/productSchema.sql
@@ -1,6 +1,5 @@
-DROP TABLE product;
 
-CREATE TABLE product (
+CREATE TABLE IF NOT EXISTS products (
      "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
      "upc" INTEGER NOT NULL,
      "category" TEXT NOT NULL,
@@ -9,13 +8,13 @@ CREATE TABLE product (
      "inventory" INTEGER NOT NULL,
      "checkId" INTEGER not null
 );
-CREATE INDEX "upcindex" ON product (upc
+CREATE INDEX "upcindex" ON products (upc
 COLLATE NOCASE ASC);
-CREATE INDEX "productnameindex" ON product (productname
+CREATE INDEX "productnameindex" ON products (productname
 COLLATE NOCASE ASC);
-INSERT INTO product(upc, category, productname, price, inventory, checkId)
+INSERT INTO products(upc, category, productname, price, inventory, checkId)
 VALUES
-  ("123451", "M", "DB iPad 4 Mini", 50001, 3, 1,),
+  ("123451", "M", "DB iPad 4 Mini", 50001, 3, 1),
   ("123452", "M", "DB H&M T-Shirt White", 1099, 10, 0),
   ("123453", "F", "DB Milk Shake", 299, 5, 0),
   ("123454", "F", "DB Pizza", 350, 20, 0),
