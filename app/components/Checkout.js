36ff5754aa8cc4f4e7cbcc39b9774110259fe0bd
3ygun <dsoller@updox.com>
2018-04-09 21:10:35 -0400

    [Checkout.js] - Switch to Link to for checkout

--- app/components/Checkout.js
+++ app/components/Checkout.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styles from '../components/TradingPost.css';
 import { connect } from 'react-redux';
 import { checkout } from '../actions/pointOfSale';
+import { Link } from 'react-router-dom';
 
 // const Checkout = ({ products, tax, total, onCheckoutClicked, onRestrictionClicked hasRestriction}) => {
 
@@ -10,17 +11,18 @@ const Checkout = ({ products, checkout }) => {
   const hasProducts = products.length > 0
 
   return (
-    <div >
-      <button 
+    <Link
+      to="/payment"
       onClick={() => checkout(products)}
-        disabled={hasProducts ? '' : 'disabled'}>
+      disabled={hasProducts ? '' : 'disabled'}>
+      <div className="checkout-btn">
         Checkout
-      </button>
-    </div>
+      </div>
+    </Link>
   )
 }
 
-  
+
 Checkout.propTypes = {
   products: PropTypes.array,
   checkout: PropTypes.func.isRequired,
