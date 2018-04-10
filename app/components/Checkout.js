80f63978a0cb93371db3ebb7feedb2a61433211b
3ygun <dsoller@updox.com>
2018-04-09 22:28:24 -0400

    [Checkout.js] - Add all payment methods

--- app/components/Checkout.js
+++ app/components/Checkout.js
@@ -4,21 +4,29 @@ import styles from '../components/TradingPost.css';
 import { connect } from 'react-redux';
 import { checkout } from '../actions/pointOfSale';
 import { Link } from 'react-router-dom';
+import { Row, Col } from 'reactstrap';
 
 // const Checkout = ({ products, tax, total, onCheckoutClicked, onRestrictionClicked hasRestriction}) => {
 
 const Checkout = ({ products, checkout }) => {
-  const hasProducts = products.length > 0
-
-  return (
+  const hasProducts = products.length > 0;
+  const linkFor = (label) => (
     <Link
+      className="checkout-btn"
       to="/payment"
       onClick={() => checkout(products)}
       disabled={hasProducts ? '' : 'disabled'}>
-      <div className="checkout-btn">
-        Checkout
-      </div>
+      <li>{label}</li>
     </Link>
+  );
+
+  return (
+    <ul className="checkout-list">
+      {linkFor('Cash')}
+      {linkFor('Credit Card')}
+      {linkFor('Check')}
+      {linkFor('Unit Account')}
+    </ul>
   )
 }
 
