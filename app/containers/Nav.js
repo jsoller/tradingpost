ba130d9df143312777f90b547e665fead140e970
jsoller <jsoller5354@gmail.com>
2018-04-29 20:45:53 -0400

    dynamic header
    header for point of sale comes from location table

--- app/containers/Nav.js
+++ app/containers/Nav.js
@@ -1,35 +1,41 @@
-// @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { getProductsIPC } from '../actions/ipcHandler';
 import { Navbar, Nav, NavItem } from 'reactstrap';
 import * as pages from '../constants/Pages';
+import { connect } from 'react-redux';
 
-type Props = {};
-
-export default class Home extends Component<Props> {
-    props: Props;
-
-    render() {
-        return (
-            <Navbar color="light" light expand="md">
-              <img src="https://247scouting.com/web/BSA999/attachment/image_14327325560_2271.gif" alt="Black Pug" />
-              <h2><Link to={pages.LOGIN}>Trading Post</Link></h2>
-              <Nav className="ml-auto" navbar>
-                <NavItem>
-                  <Link to={pages.POINT_OF_SALE}>Point of Sale</Link>
-                </NavItem>
-                <NavItem>
-                  <Link to={pages.PAYMENT}>Payment</Link>
-                </NavItem>
-                <NavItem>
-                  <Link to={pages.INVENTORY}>Inventory</Link>
-                </NavItem>
-                <NavItem onClick={() => getProductsIPC('productByType', 'F')}>
-                  <span>Refresh</span>
-                </NavItem>
-              </Nav>
-            </Navbar>
-        );
-    }
+class TPNav extends Component {
+  render() {
+    return (
+      <Navbar color="light" light expand="md">
+        <img src="https://247scouting.com/web/BSA999/attachment/image_14327325560_2271.gif" alt="Black Pug" />
+        <h2><Link to={pages.LOGIN}>
+          {this.props.locationName}
+        </Link></h2>
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <Link to={pages.POINT_OF_SALE}>Point of Sale</Link>
+          </NavItem>
+          <NavItem>
+            <Link to={pages.PAYMENT}>Payment</Link>
+          </NavItem>
+          <NavItem>
+            <Link to={pages.INVENTORY}>Inventory</Link>
+          </NavItem>
+          <NavItem onClick={() => getProductsIPC('productByType', 'F')}>
+            <span>Refresh</span>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    );
+  }
 }
+
+const mapStateToProps = (state) => ({
+  locationName: state.login.location.nme ? state.login.location.nme : 'Trading Post',
+});
+
+export default connect(
+  mapStateToProps
+)(TPNav);
